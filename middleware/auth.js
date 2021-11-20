9edba277f682fb56c48e6c747fea7aabff3868b1
pkaplan <pkaplan1@seznam.cz>
2021-11-20 17:24:09 +0100

    fixing emailing

--- middleware/auth.js
+++ middleware/auth.js
@@ -5,7 +5,7 @@ const auth = async (req, res, next) => {
     const token = req.headers.authorization.split(" ")[1];
     let decodedData;
     if (token) {
-      decodedData = jwt.verify(token, "test");
+      decodedData = jwt.verify(token, process.env.JWT_PASS);
       req.userId = decodedData?.id;
     } else {
       decodedData = jwt.decode(token);
